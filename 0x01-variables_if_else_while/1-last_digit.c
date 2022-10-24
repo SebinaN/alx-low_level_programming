6afdea6a18806843194a553dc410524faf75fb23
Sebina Nong <sebinanong@gmail.com>
2022-10-24 02:18:30 -0700

    Task_1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,8 +27,9 @@ int main(void)
 	}
 	if (x < 6 && x != 0)
 	{
-		printf("Last digit of -98 is -8 and is less than 6 and not 0");
+		printf("and is less than 6 and not 0");
 	}
+
 	printf("\n");
 
 	return (0);

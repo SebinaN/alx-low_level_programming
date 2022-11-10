9906e51ded58502c4f542f8274d97051e7c72584
Sebina Nong <sebinanong@gmail.com>
2022-11-10 09:38:55 -0800

    Task_0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 void _print(char *str, int l);
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
 #include <stdio.h>

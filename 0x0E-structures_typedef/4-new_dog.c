ca606c75d1e1821dc2525c5438ec836e428a6679
Sebina Nong <sebinanong@gmail.com>
2022-11-14 13:36:07 -0800

    Task_4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,6 +33,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->age = age;
 	for (ol = 0; owner[ol]; ol++)
 		;
+	ol++;
 	dog->owner = malloc(ol * sizeof(char));
 	if (dog->owner == NULL)
 	{
